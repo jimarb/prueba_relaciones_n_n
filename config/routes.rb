9128ecb610dafc99f664809317e0747241203cac
Jose <jimarb@outlook.com>
2019-09-01 18:13:56 -0400

    Fix the routes with the custom controller of registration user, now yo can save the name of the user

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     resources :todousers, only: [:create, :destroy]
   end
   root to: 'todos#index'
-  devise_for :users
+  devise_for :users, controllers: {
+      registrations: 'users/registrations'
+    }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
